fc6c6b00ea60e71470ded6e42438a2f7df23c1d0
Dmitry Makarenko <kryksyh@gmail.com>
2018-08-25 16:15:02 +0300

    Refactoring

--- CiaServer.py
+++ CiaServer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import qrcode
-from tempfile import NamedTemporaryFile
 import tkinter as tk
+from PIL import ImageTk
+
 import http.server as server
 import random
 from threading import Thread
@@ -10,22 +11,24 @@ import os
 import urllib.parse
 import socket
 
-ip = socket.gethostbyname(socket.gethostname())
 
 if len(sys.argv) < 2:
     print(f'Usage:\n $ {sys.argv[0]} /path/to/game.cia')
     exit(1)
 
 # If no external IP was detected there is no point to continue
+ip = socket.gethostbyname(socket.gethostname())
 if not len(ip) or ip.split('.')[0] == '127':
     print('Failed to detect external ip address. Sorry for that.')
     exit(1)
+
 
 # Moving to cia's location
 cia = sys.argv[1]
 cia_name = cia.split(os.sep)[-1]
 cia_path = cia.replace(cia_name,'')
 os.chdir(cia_path)
+
 
 # Binding to random port to allow multiple copies running
 # there is slim chance to fail here, but i'll leave it as it is
@@ -41,17 +44,17 @@ s = server.HTTPServer(('',port), handler)
 thread = Thread(target=s.serve_forever)
 thread.start()
 
-# Creating QR Code image and saving it in temp file
-img = qrcode.make(url)
-tmp = NamedTemporaryFile()
-img.save(tmp)
 
 # Creating window to show qr code
 root = tk.Tk()
-canvas = tk.Canvas(root, width=img.height, height=img.height)
-canvas.pack()
-img = tk.PhotoImage(file=tmp.name)
+
+# Creating QR Code image 
+img = ImageTk.PhotoImage(qrcode.make(url))
+
+canvas = tk.Canvas(root, width=img.width(), height=img.height())
 canvas.create_image(0, 0, anchor=tk.NW, image=img)
+
+canvas.pack()
 tk.mainloop()
 
 # Shutting down server after image was closed
